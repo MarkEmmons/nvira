6ef5e984419ab696de0430c675d6cbfd0e54a70f
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-10-06 13:36:42 -0500

    Re-enable luasnip since cmp requires a snippet engine

--- nvim/.config/nvim/lua/mark/cmp.lua
+++ nvim/.config/nvim/lua/mark/cmp.lua
@@ -3,6 +3,11 @@ if not cmp_status_ok then
 	return
 end
 
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+	return
+end
+
 -- Add some kind_icons
 --   פּ ﯟ   some other good icons
 local kind_icons = {
@@ -35,15 +40,19 @@ local kind_icons = {
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 -- cmp.setup
--- Hold tab to scroll through completion menu.
--- Let go to select
+-- Hold alt to scroll through completion menu.
 cmp.setup {
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
 	mapping = {
 		["<A-k>"] = cmp.mapping.select_prev_item(),
 		["<A-j>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<A-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-y>"] = cmp.config.disable,
 		["<C-e>"] = cmp.mapping {
 			i = cmp.mapping.abort(),
--- nvim/.config/nvim/lua/mark/plugins.lua
+++ nvim/.config/nvim/lua/mark/plugins.lua
@@ -54,6 +54,9 @@ return packer.startup(function(use)
 	use "hrsh7th/cmp-nvim-lsp"
 	use "hrsh7th/cmp-nvim-lua"
 
+	-- snippets
+	use "L3MON4D3/LuaSnip" --snippet engine
+
 	-- LSP
 	use "neovim/nvim-lspconfig"
 	use "williamboman/nvim-lsp-installer"
