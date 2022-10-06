c7d8ed3b0a2ecbe2a7892d80d83ea77169066db1
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-10-06 16:10:55 -0500

    Disable preselect for nvim-cmp

--- nvim/.config/nvim/lua/mark/cmp.lua
+++ nvim/.config/nvim/lua/mark/cmp.lua
@@ -50,37 +50,36 @@ cmp.setup {
 	mapping = {
 		["<A-k>"] = cmp.mapping.select_prev_item(),
 		["<A-j>"] = cmp.mapping.select_next_item(),
-		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<A-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<A-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<A-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable,
-		["<C-e>"] = cmp.mapping {
+		["<A-y>"] = cmp.config.disable,
+		["<A-e>"] = cmp.mapping {
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		},
-		-- Accept currently selected item. If none selected, 'select' first item.
-		-- Set 'select' to 'false' to only confirm explicitly selected items.
+		-- Only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm { select = false },
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
+		--["<Tab>"] = cmp.mapping(function(fallback)
+		--	if cmp.visible() then
+		--		cmp.select_next_item()
+		--	else
+		--		fallback()
+		--	end
+		--end, {
+		--	"i",
+		--	"s",
+		--}),
+		--["<S-Tab>"] = cmp.mapping(function(fallback)
+		--	if cmp.visible() then
+		--		cmp.select_prev_item()
+		--	else
+		--		fallback()
+		--	end
+		--end, {
+		--	"i",
+		--	"s",
+		--}),
 	},
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
@@ -101,6 +100,8 @@ cmp.setup {
 		{ name = "buffer" },
 		{ name = "path" },
 	},
+	-- Do not auto-select the first item in the list
+	preselect = cmp.PreselectMode.None,
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
