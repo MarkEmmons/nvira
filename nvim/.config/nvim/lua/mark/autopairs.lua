375cd5ce446dd2676500c822ad643f5cfcf2c067
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-10-08 16:39:33 -0500

    Disable autopairs for ' in rust

--- nvim/.config/nvim/lua/mark/autopairs.lua
+++ nvim/.config/nvim/lua/mark/autopairs.lua
@@ -3,6 +3,11 @@ if not status_ok then
 	return
 end
 
+local rule_status_ok, Rule = pcall(require, "nvim-autopairs.rule")
+if not rule_status_ok then
+	return
+end
+
 npairs.setup {
 	check_ts = true,
 	ts_config = {
@@ -22,6 +27,11 @@ npairs.setup {
 		highlight = "PmenuSel",
 		highlight_grey = "LineNr",
 	},
+}
+
+npairs.remove_rule("'")
+npairs.add_rules {
+	(Rule("'", "'", "-rust"))
 }
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
