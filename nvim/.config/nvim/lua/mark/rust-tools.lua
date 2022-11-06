f1099dd51ecefbbc22975f27cf3e559d5eadfec9
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-11-06 02:26:31 -0600

    Add keymaps for inlay hints

--- nvim/.config/nvim/lua/mark/rust-tools.lua
+++ nvim/.config/nvim/lua/mark/rust-tools.lua
@@ -4,11 +4,24 @@ if not rust_ok then
 end
 
 rt.setup({
+
 	server = {
 		on_attach = function(client, bufnr)
 
+			local keymap = vim.api.nvim_buf_set_keymap
+			local opts = { noremap = true, silent = true }
+
 			require("mark.lsp.handlers").LspKeymaps(bufnr)
 			require("mark.lsp.handlers").LspHighlightDocument(client)
+
+			keymap(bufnr, "n", "<Leader>1", "<cmd>RustEnableInlayHints<CR>", opts)
+			keymap(bufnr, "n", "<Leader>2", "<cmd>RustDisableInlayHints<CR>", opts)
+			keymap(bufnr, "n", "<Leader>3", "<cmd>RustSetInlayHints<CR>", opts)
+			keymap(bufnr, "n", "<Leader>4", "<cmd>RustUnsetInlayHints<CR>", opts)
 		end,
+	},
+
+	inlay_hints = {
+		auto = false,
 	},
 })
