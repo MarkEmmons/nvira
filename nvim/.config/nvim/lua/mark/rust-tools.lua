30e7fd705957360d1dcd612f7e60de769ec713e6
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-11-07 16:31:49 -0600

    Add nvim-dap configuration to rust-tools

--- nvim/.config/nvim/lua/mark/rust-tools.lua
+++ nvim/.config/nvim/lua/mark/rust-tools.lua
@@ -3,13 +3,19 @@ if not rust_ok then
 	return
 end
 
+local opts = { noremap = true, silent = true }
+
 rt.setup({
 
 	server = {
+
+		inlay_hints = {
+			auto = false,
+		},
+
 		on_attach = function(client, bufnr)
 
 			local keymap = vim.api.nvim_buf_set_keymap
-			local opts = { noremap = true, silent = true }
 
 			require("mark.lsp.handlers").LspKeymaps(bufnr)
 			require("mark.lsp.handlers").LspHighlightDocument(client)
@@ -20,8 +26,44 @@ rt.setup({
 			keymap(bufnr, "n", "<Leader>4", "<cmd>RustUnsetInlayHints<CR>", opts)
 		end,
 	},
-
-	inlay_hints = {
-		auto = false,
-	},
 })
+
+rt.inlay_hints.disable()
+rt.inlay_hints.unset()
+
+local dap_ok, dap = pcall(require, "dap")
+if not dap_ok then
+	return
+end
+
+dap.adapters.lldb = {
+	type = 'executable',
+	command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+	name = 'lldb'
+}
+
+local configurations = {
+	name = 'Launch',
+	type = 'lldb',
+	request = 'launch',
+	program = function()
+	return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+	end,
+	cwd = '${workspaceFolder}',
+	stopOnEntry = false,
+	args = {},
+}
+
+dap.configurations.rust = { configurations }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<F5>", "<Cmd>lua require'dap'.continue()<CR>", opts)
+keymap("n", "<F10>", "<Cmd>lua require'dap'.step_over()<CR>", opts)
+keymap("n", "<F11>", "<Cmd>lua require'dap'.step_into()<CR>", opts)
+keymap("n", "<F12>", "<Cmd>lua require'dap'.step_out()<CR>", opts)
+keymap("n", "<Leader>b", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
+--keymap("n", "<Leader>B", "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
+--keymap("n", "<Leader>lp", "<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", opts)
+keymap("n", "<Leader>dr", "<Cmd>lua require'dap'.repl.open()<CR>", opts)
+--keymap("n", "<Leader>dl", "<Cmd>lua require'dap'.run_last()<CR>", opts)
