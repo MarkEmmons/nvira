52383408b29ea1fd0bd874283a01c137a436c75b
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-11-09 16:50:10 -0600

    Add mvp test dubgging for Rust

--- nvim/.config/nvim/lua/mark/dap/keymaps.lua
+++ nvim/.config/nvim/lua/mark/dap/keymaps.lua
@@ -1,6 +1,10 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+-- Get nearest test							[x]
+-- Get test binary							[x]
+-- Call test binary with --test <test_name>	[]
+
 keymap("n", "<F5>", "<Cmd>lua require'dap'.continue()<CR>", opts)
 keymap("n", "<F10>", "<Cmd>lua require'dap'.step_over()<CR>", opts)
 keymap("n", "<F11>", "<Cmd>lua require'dap'.step_into()<CR>", opts)
@@ -9,3 +13,4 @@ keymap("n", "<Leader>b", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
 
 keymap("n", "<Leader>D", "<Cmd>lua require'dapui'.toggle()<CR>", opts)
 
+keymap("n", "<Leader>DT", "<Cmd>lua require'dapui'.toggle()<CR>", opts)
