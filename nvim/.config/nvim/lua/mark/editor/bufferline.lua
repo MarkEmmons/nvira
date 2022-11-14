0544f17e082202ab31f71980f1486b61dfd5bbb6
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-11-13 19:00:46 -0600

    Clean up bufferline config

--- nvim/.config/nvim/lua/mark/editor/bufferline.lua
+++ nvim/.config/nvim/lua/mark/editor/bufferline.lua
@@ -3,142 +3,96 @@ if not status_ok then
 	return
 end
 
-bufferline.setup {
+local function run_custom_filter(buf_number, _)
 
-	options = {
-		numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-		-- NOTE: this plugin is designed with this icon in mind,
-		-- and so changing this is NOT recommended, this is intended
-		-- as an escape hatch for people who cannot bear it for whatever reason
-		indicator = {
-			style = "icon",
-			icon = "▎",
-		},
-		buffer_close_icon = "",
-		-- buffer_close_icon = '',
-		modified_icon = "●",
-		close_icon = "",
-		-- close_icon = '',
-		left_trunc_marker = "",
-		right_trunc_marker = "",
-		max_name_length = 30,
-		max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-		tab_size = 21,
-		diagnostics = false, -- | "nvim_lsp" | "coc",
-		diagnostics_update_in_insert = false,
-		custom_filter = function(buf_number, buf_numbers)
-			-- filter out by buffer name
-			if vim.fn.bufname(buf_number) ~= "[dap-repl]" and
-				vim.fn.bufname(buf_number) ~= "[No Name]" then
+	-- filter out by buffer name
+	if vim.fn.bufname(buf_number) ~= "[dap-repl]" and
+		vim.fn.bufname(buf_number) ~= "[No Name]" then
 
-				return true
-			end
-		end,
-		offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-		show_buffer_icons = true,
-		show_buffer_close_icons = true,
-		show_close_icon = true,
-		show_tab_indicators = true,
-		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-		separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-		enforce_regular_tabs = true,
-		always_show_bufferline = true,
+		return true
+	end
+end
+
+--local function run_name_formatter(path)
+--
+--	print(path)
+--	return path
+--end
+
+local opts = {
+
+	-- Use vimbbye for close commands
+	close_command = "Bdelete! %d",
+	right_mouse_command = "Bdelete! %d",
+
+	-- Icons
+	indicator = {
+		style = "icon",
+		icon = "▎",
 	},
-	highlights = {
+	buffer_close_icon = "",
+	modified_icon = "●",
+	close_icon = "",
+	left_trunc_marker = "",
+	right_trunc_marker = "",
 
-		fill = {
-			fg = { attribute = "fg", highlight = "#ff0000" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		background = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
+	-- Naming
+	--max_name_length = 30,
+	--max_prefix_length = 30,
 
-		-- buffer_selected = {
-		--   fg = {attribute='fg',highlight='#ff0000'},
-		--   bg = {attribute='bg',highlight='#0000ff'},
-		--   },
-		buffer_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
+	-- Diagnostics
+	diagnostics = false, -- set to "nvim_lsp" for lsp
+	diagnostics_update_in_insert = false,
 
-		close_button = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		close_button_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		-- close_button_selected = {
-		--   fg = {attribute='fg',highlight='TabLineSel'},
-		--   bg ={attribute='bg',highlight='TabLineSel'}
-		--   },
+	-- Filter out buffers created by other plugins
+	custom_filter = run_custom_filter,
+	--name_formatter = run_name_formatter,
 
-		tab_selected = {
-			fg = { attribute = "fg", highlight = "Normal" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-		tab = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		tab_close = {
-			-- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
-			fg = { attribute = "fg", highlight = "TabLineSel" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-
-		duplicate_selected = {
-			fg = { attribute = "fg", highlight = "TabLineSel" },
-			bg = { attribute = "bg", highlight = "TabLineSel" },
-			italic = true,
-		},
-		duplicate_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-			italic = true,
-		},
-		duplicate = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-			italic = true,
-		},
-
-		modified = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		modified_selected = {
-			fg = { attribute = "fg", highlight = "Normal" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-		modified_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-
-		separator = {
-			fg = { attribute = "bg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		separator_selected = {
-			fg = { attribute = "bg", highlight = "Normal" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-		-- separator_visible = {
-		--   fg = {attribute='bg',highlight='TabLine'},
-		--   bg = {attribute='bg',highlight='TabLine'}
-		--   },
-		indicator_selected = {
-			fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
+	-- Offsets: TODO: dap-ui
+	offsets = {
+		{ filetype = "NvimTree", text = "", padding = 1 },
+		{ filetype = "dapui_breakpoints", text = "", padding = 1 },
 	},
 }
+
+local function bl_expand_highlight(highlight)
+	return {
+		fg = { attribute = "fg", highlight = highlight.fg },
+		bg = { attribute = "bg", highlight = highlight.bg },
+		italic = highlight.italic,
+	}
+end
+
+local hl = {
+
+	background        = bl_expand_highlight({ fg = "TabLine", bg = "TabLine"}),
+
+	--buffer_selected = bl_expand_highlight({ fg = "TabLine", bg = "TabLine"}),
+	buffer_visible    = bl_expand_highlight({ fg = "TabLine", bg = "TabLine"}),
+
+	close_button            = bl_expand_highlight({ fg = "TabLine", bg = "TabLine" }),
+	--close_button_selected = bl_expand_highlight({ fg = 'TabLineSel', bg = 'TabLineSel' }),
+	close_button_visible    = bl_expand_highlight({ fg = "TabLine", bg = "TabLine" }),
+
+	duplicate          = bl_expand_highlight({ fg = "TabLine", bg = "TabLine", italic = true }),
+	duplicate_selected = bl_expand_highlight({ fg = "TabLine", bg = "TabLine", italic = true }),
+	duplicate_visible  = bl_expand_highlight({ fg = "TabLine", bg = "TabLine", italic = true }),
+
+	fill = bl_expand_highlight({ fg = "TabLine", bg = "TabLine"}),
+
+	indicator_selected = bl_expand_highlight({ fg = "LspDiagnosticsDefaultHint", bg = "Normal" }),
+
+	modified          = bl_expand_highlight({ fg = "TabLine", bg = "TabLine" }),
+	modified_selected = bl_expand_highlight({ fg = "Normal", bg = "Normal" }),
+	modified_visible  = bl_expand_highlight({ fg = "TabLine", bg = "TabLine" }),
+
+	separator           = bl_expand_highlight({ fg = "TabLine", bg = "TabLine" }),
+	separator_selected  = bl_expand_highlight({ fg = "Normal", bg = "Normal" }),
+	--separator_visible = bl_expand_highlight({ fg = 'TabLine', bg = 'TabLine' }),
+
+	tab          = bl_expand_highlight({ fg = "TabLine", bg = "TabLine" }),
+	tab_selected = bl_expand_highlight({ fg = "Normal", bg = "Normal" }),
+	tab_close    = bl_expand_highlight({ fg = "TabLineSel", bg = "Normal" }),
+}
+
+bufferline.setup({ options = opts, highlights = hl })
