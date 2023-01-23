5fa1c9961fb14d46c762bee0a56fb4f9ce352127
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2023-01-23 15:33:06 -0600

    Configure the jedi language server for python projects

--- lua/nvira/lang/lsp/init.lua
+++ lua/nvira/lang/lsp/init.lua
@@ -11,11 +11,13 @@ local opts = {
 local sumneko_opts = require("nvira.lang.lsp.settings.sumneko_lua")
 local bashls_opts = require("nvira.lang.lsp.settings.bashls")
 local denols_opts = require("nvira.lang.lsp.settings.denols")
+local jedils_opts = require("nvira.lang.lsp.settings.jedils")
 local texlab_opts = require("nvira.lang.lsp.settings.texlab")
 
 nvim_lsp['sumneko_lua'].setup(vim.tbl_deep_extend("force", sumneko_opts, opts))
 nvim_lsp['bashls'].setup(vim.tbl_deep_extend("force", bashls_opts, opts))
 nvim_lsp['denols'].setup(vim.tbl_deep_extend("force", denols_opts, opts))
+nvim_lsp['jedi_language_server'].setup(vim.tbl_deep_extend("force", jedils_opts, opts))
 nvim_lsp['texlab'].setup(vim.tbl_deep_extend("force", texlab_opts, opts))
 
 -- Augment vim.g.markdown_fenced_languages to appropriately highlight codefences returned from denols 
